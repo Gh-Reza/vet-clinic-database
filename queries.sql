ede94d0e058b97449ce749eaf9c69e1c452047dd
Reza Merzaie <ghrezam97@gmail.com>
2023-08-15 23:30:12 +0430

    Add quereies

--- queries.sql
+++ queries.sql
@@ -15,3 +15,41 @@ SELECT * FROM animals WHERE neutered = true;
 SELECT * FROM animals WHERE NOT name = 'Gabumon';
 
 SELECT * FROM animals WHERE weight >= 10.4 AND weight <= 17.3;
+
+-- Transactions
+
+BEGIN;
+UPDATE animals SET species = 'unspecified';
+SELECT * FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
+BEGIN;
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
+SELECT * FROM animals;
+COMMIT;
+SELECT * FROM animals;
+
+BEGIN;
+DELETE FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
+
+
+BEGIN;
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+SAVEPOINT first_savepoint;
+UPDATE animals SET weight = weight * -1;
+ROLLBACK TO SAVEPOINT first_savepoint;
+UPDATE animals SET weight = weight * -1 WHERE weight < 0;
+COMMIT;
+
+-- Queries for questions
+
+SELECT COUNT(*) FROM animals; 
+SELECT COUNT(*) FROM animals WHERE escape_attempt = 0; 
+SELECT AVG(weight) AS weight_average FROM animals; 
+SELECT neutered, MAX(escape_attempt) FROM animals GROUP BY neutered; 
+SELECT neutered, MAX(weight) as max_weight, MIN(weight) as min_weight FROM animals GROUP BY neutered; 
+SELECT neutered, AVG(escape_attempt) AS avg_escape_attempt FROM animals WHERE date_of_birth > '1990-01-01' AND date_of_birth < '2000-01-01' GROUP BY neutered; 
+
