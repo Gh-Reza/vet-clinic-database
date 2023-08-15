5a9c985580837334e127ff2ba11db9f2561f0946
Reza Merzaie <ghrezam97@gmail.com>
2023-08-15 22:33:02 +0430

    Inserted more sample data

--- data.sql
+++ data.sql
@@ -34,4 +34,69 @@ VALUES (
 	5,
 	true,
 	11
+);
+
+INSERT INTO animals (
+	id,
+	name,
+	date_of_birth,
+	escape_attempt,
+	neutered,
+	weight
 )
+VALUES (
+	5,
+	'Charmander',
+	'2020-02-08',
+	0,
+	true,
+	-11
+),
+(
+	6,
+	'Plantmon',
+	'2021-11-15',
+	2,
+	true,
+	-5.7
+),
+(
+	7,
+	'Squirtle',
+	'1993-04-02',
+	3,
+	false,
+	-12.13
+),
+(
+	8,
+	'Angemon',
+	'2005-06-12',
+	1,
+	true,
+	-45
+),
+(
+	9,
+	'Boarmon',
+	'2005-06-07',
+	7,
+	true,
+	20.4
+),
+(
+	10,
+	'Blossom',
+	'1998-10-13',
+	3,
+	true,
+	17
+),
+(
+	11,
+	'Ditto',
+	'2022-05-14',
+	4,
+	true,
+	22
+);
