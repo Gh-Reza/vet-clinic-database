a76b8e7e348bc35e54fb0a9538c649f08786b806
Reza Merzaie <ghrezam97@gmail.com>
2023-08-16 22:15:55 +0430

    Insert data into owners table

--- data.sql
+++ data.sql
@@ -100,3 +100,34 @@ VALUES (
 	true,
 	22
 );
+
+-- Insert data into owners table
+
+INSERT INTO owners (
+	full_name,
+	age
+)
+VALUES (
+	'Sam Smith',
+	34
+),
+(
+	'Jennifer Orwell',
+	19
+),
+(
+	'Bob',
+	45
+),
+(
+	'Melody Pond',
+	77
+),
+(
+	'Dean Winchester',
+	14
+),
+(
+	'Jodie Whittaker',
+	38
+)
