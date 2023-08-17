70d0f3c31330cd93767a9401d838b726cd62bd57
Reza Merzaie <ghrezam97@gmail.com>
2023-08-17 22:37:35 +0430

    Created the vets and specialization tables

--- schema.sql
+++ schema.sql
@@ -37,3 +37,20 @@ ALTER TABLE animals ADD COLUMN species_id INT;
 ALTER TABLE animals ADD CONSTRAINT fk_species_id FOREIGN KEY (species_id) REFERENCES species(id);
 ALTER TABLE animals ADD COLUMN owner_id INT;
 ALTER TABLE animals ADD CONSTRAINT fk_owner_id FOREIGN KEY (owner_id) REFERENCES owners(id);
+
+CREATE TABLE vets (
+	id SERIES PRIMARY KEY,
+	name VARCHAR(50),
+	age INT,
+	date_of_graduation DATE
+);
+
+CREATE TABLE specializations (
+	species_id INT,
+	vets_id INT,
+	CONSTRAINT fk_species_id FOREIGN KEY(species_id) REFERENCES species(id),
+	CONSTRAINT fk_vets_id FOREIGN KEY(vets_id) REFERENCES vets(id),
+	PRIMARY KEY (species_id, vets_id)
+);
+
+
